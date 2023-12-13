f7a7eb5271a792063e4c8c2cb16274704c7609a4
Daniel Santana <danielgsantana@gmail.com>
2023-12-13 16:58:50 +0000

    Speedup day 11

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -3,35 +3,65 @@ use std::{fmt::Display, vec};
 
 use super::Solution;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+#[derive(Debug, Eq, Clone, Copy)]
 enum Space {
     Empty,
-    Galaxy(usize, usize, usize),
+    Galaxy(usize, usize),
+}
+
+impl PartialEq for Space {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Galaxy(x1, y1), Self::Galaxy(x2, y2)) => x1 == x2 && y1 == y2,
+            _ => core::mem::discriminant(self) == core::mem::discriminant(other),
+        }
+    }
+}
+
+#[derive(Debug, Eq, Clone, Copy)]
+struct Pair {
+    galaxy1: Space,
+    galaxy2: Space,
+}
+
+impl Pair {
+    fn new(galaxy1: Space, galaxy2: Space) -> Option<Self> {
+        if galaxy1 == galaxy2 {
+            return None;
+        }
+        Some(Self { galaxy1, galaxy2 })
+    }
+}
+
+impl PartialEq for Pair {
+    /// We don't care about the order of the galaxies, only check if the pair contains the same galaxies
+    /// Which is the same as checking if the coordinates are the same.
+    fn eq(&self, other: &Self) -> bool {
+        self.galaxy1 == other.galaxy1 && self.galaxy2 == other.galaxy2
+            || self.galaxy1 == other.galaxy2 && self.galaxy2 == other.galaxy1
+    }
 }
 
 impl Space {
-    fn from_char(c: char, index: &mut usize, x: usize, y: usize) -> Self {
+    fn from_char(c: char, x: usize, y: usize) -> Self {
         match c {
             '.' => Self::Empty,
-            '#' => {
-                *index += 1;
-                Self::Galaxy(*index, x, y)
-            }
+            '#' => Self::Galaxy(x, y),
             _ => panic!("Invalid space"),
         }
     }
 
     fn get_x(&self) -> usize {
         match self {
-            Self::Empty => panic!("Empty space has no coordinates"),
-            Self::Galaxy(_, x, _) => *x,
+            Self::Empty => 0,
+            Self::Galaxy(x, _) => *x,
         }
     }
 
     fn get_y(&self) -> usize {
         match self {
-            Self::Empty => panic!("Empty space has no coordinates"),
-            Self::Galaxy(_, _, y) => *y,
+            Self::Empty => 0,
+            Self::Galaxy(_, y) => *y,
         }
     }
 }
@@ -40,7 +70,7 @@ impl Display for Space {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Empty => write!(f, "."),
-            Self::Galaxy(_, _, _) => write!(f, "#"),
+            Self::Galaxy(_, _) => write!(f, "#"),
         }
     }
 }
@@ -48,71 +78,48 @@ impl Display for Space {
 #[derive(Debug)]
 struct Universe {
     grid: Vec<Vec<Space>>,
+    galaxies: Vec<Space>,
     rows: Vec<usize>,
     cols: Vec<usize>,
     width: usize,
+    #[allow(unused)]
     height: usize,
-    distances: Vec<(Space, Space, usize)>,
 }
 
 impl Universe {
     fn from_str(input: &str) -> Self {
-        let mut galaxies = 0;
         let grid = input
             .lines()
             .enumerate()
             .map(|(y, line)| {
                 line.chars()
                     .enumerate()
-                    .map(|(x, c)| Space::from_char(c, &mut galaxies, x, y))
+                    .map(|(x, c)| Space::from_char(c, x, y))
                     .collect()
             })
             .collect::<Vec<Vec<Space>>>();
+        let galaxies = grid
+            .iter()
+            .flatten()
+            .filter(|space| matches!(space, Space::Galaxy(_, _)))
+            .cloned()
+            .collect::<Vec<Space>>();
+
         let width = grid[0].len();
         let height = grid.len();
         Self {
             grid,
+            galaxies,
             rows: vec![1; height],
             cols: vec![1; width],
             width,
             height,
-            distances: Vec::new(),
         }
     }
 
     /// Expands the universe for each row or column that are empty.
     ///
     /// So an empty row causes to duplicate that row. An empty column duplicates that column.
-    fn expand_universe(&mut self) {
-        let mut new_grid = Vec::new();
-        for row in self.grid.iter() {
-            if self.is_empty_row(row) {
-                new_grid.push(row.clone());
-                new_grid.push(row.clone());
-            } else {
-                new_grid.push(row.clone());
-            }
-        }
-        for col in (0..self.width).rev() {
-            if new_grid.iter().all(|row| row[col] == Space::Empty) {
-                for row in new_grid.iter_mut() {
-                    row.insert(col, Space::Empty);
-                }
-            }
-        }
-        // Update width and height and galaxy coordinates
-        self.width = new_grid[0].len();
-        self.height = new_grid.len();
-        for (y, row) in new_grid.iter_mut().enumerate() {
-            for (x, space) in row.iter_mut().enumerate() {
-                if let Space::Galaxy(index, _, _) = space {
-                    *space = Space::Galaxy(*index, x, y);
-                }
-            }
-        }
-        self.grid = new_grid;
-    }
-
     fn older_expand_universe(&mut self, times: usize) {
         for (row_index, row) in self.grid.iter().enumerate() {
             if self.is_empty_row(row) {
@@ -125,15 +132,13 @@ impl Universe {
             }
         }
 
-        for (y, row) in self.grid.iter_mut().enumerate() {
-            for (x, space) in row.iter_mut().enumerate() {
-                if let Space::Galaxy(index, _, _) = space {
-                    let new_x = self.rows[0..=y].iter().sum::<usize>();
-                    let new_y = self.cols[0..=x].iter().sum::<usize>();
-                    *space = Space::Galaxy(*index, new_x, new_y);
-                }
-            }
-        }
+        self.galaxies.iter_mut().for_each(|galaxy| {
+            let x = galaxy.get_x();
+            let y = galaxy.get_y();
+            let new_x = self.rows[0..=y].iter().sum::<usize>();
+            let new_y = self.cols[0..=x].iter().sum::<usize>();
+            *galaxy = Space::Galaxy(new_x, new_y);
+        });
     }
 
     fn is_empty_row(&self, row: &[Space]) -> bool {
@@ -141,57 +146,29 @@ impl Universe {
     }
 
     /// Calculates the shortest paths between all galaxies.
-    /// Only move up, down, left or right.
-    fn calculate_shortest_paths(&mut self) {
-        let mut distances = Vec::new();
+    fn calculate_shortest_paths(&mut self) -> usize {
+        let mut distance_total = 0;
 
-        let galaxies = self
-            .grid
+        self.galaxies
             .iter()
-            .flatten()
-            .filter(|space| matches!(space, Space::Galaxy(_, _, _)))
-            .collect::<Vec<&Space>>();
+            .combinations(2)
+            .filter_map(|v| Pair::new(*v[0], *v[1]))
+            .dedup()
+            .for_each(|pair| {
+                let distance = self.calculate_distance(
+                    pair.galaxy1.get_x(),
+                    pair.galaxy1.get_y(),
+                    pair.galaxy2.get_x(),
+                    pair.galaxy2.get_y(),
+                );
+                distance_total += distance;
+            });
 
-        for galaxy in galaxies.iter() {
-            println!("{:?}", galaxy);
-        }
-
-        for galaxy1 in galaxies.iter() {
-            if let Space::Galaxy(_, _, _) = galaxy1 {
-                for galaxy2 in galaxies.iter() {
-                    if galaxy1 == galaxy2 {
-                        continue;
-                    }
-                    if Self::has_galaxy(&distances, **galaxy1, **galaxy2) {
-                        continue;
-                    }
-                    if let Space::Galaxy(_, _, _) = galaxy2 {
-                        let distance = self.calculate_distance(
-                            galaxy1.get_x(),
-                            galaxy1.get_y(),
-                            galaxy2.get_x(),
-                            galaxy2.get_y(),
-                        );
-                        distances.push((**galaxy1, **galaxy2, distance));
-                    }
-                }
-            }
-        }
-
-        self.distances = distances;
-    }
-
-    fn has_galaxy(distances: &[(Space, Space, usize)], galaxy1: Space, galaxy2: Space) -> bool {
-        distances
-            .iter()
-            .any(|(g1, g2, _)| *g1 == galaxy1 && *g2 == galaxy2 || *g1 == galaxy2 && *g2 == galaxy1)
+        distance_total
     }
 
     /// Calculates the shortest distance between two galaxies.
     /// We can only move up, down, left or right, at each step.
-    ///
-    /// This is almost like drawing a line between the two galaxies, but we can only move
-    /// horizontally or vertically in each step, aka line drawing a pixel line.
     fn calculate_distance(&self, x1: usize, y1: usize, x2: usize, y2: usize) -> usize {
         let dx = if x1 > x2 { x1 - x2 } else { x2 - x1 };
         let dy = if y1 > y2 { y1 - y2 } else { y2 - y1 };
@@ -213,22 +190,14 @@ impl Display for Universe {
 
 fn part1(input: &str) -> i64 {
     let mut universe = Universe::from_str(input);
-    println!("{}", universe);
-    println!("expanding...");
-    universe.expand_universe();
-    println!("{}", universe);
-    universe.calculate_shortest_paths();
-    universe.distances.iter().map(|(_, _, d)| *d as i64).sum()
+    universe.older_expand_universe(2);
+    universe.calculate_shortest_paths() as i64
 }
 
 fn part2(input: &str) -> i64 {
     let mut universe = Universe::from_str(input);
-    println!("{}", universe);
-    println!("expanding...");
     universe.older_expand_universe(1000000);
-    println!("{}", universe);
-    universe.calculate_shortest_paths();
-    universe.distances.iter().map(|(_, _, d)| *d as i64).sum()
+    universe.calculate_shortest_paths() as i64
 }
 
 pub struct Day11;
@@ -268,7 +237,8 @@ mod tests {
 
     #[test]
     fn test_part2_by10() {
-        let mut universe = Universe::from_str("...#......
+        let mut universe = Universe::from_str(
+            "...#......
 .......#..
 #.........
 ..........
@@ -277,22 +247,20 @@ mod tests {
 .........#
 ..........
 .......#..
-#...#.....");
+#...#.....",
+        );
         println!("{}", universe);
         println!("expanding...");
         universe.older_expand_universe(10);
         println!("{}", universe);
-        universe.calculate_shortest_paths();
-        let sum: i64 = universe.distances.iter().map(|(_, _, d)| *d as i64).sum();
-        assert_eq!(
-            sum,
-            1030
-        );
+        let sum = universe.calculate_shortest_paths();
+        assert_eq!(sum, 1030);
     }
 
     #[test]
     fn test_part2_by100() {
-        let mut universe = Universe::from_str("...#......
+        let mut universe = Universe::from_str(
+            "...#......
 .......#..
 #.........
 ..........
@@ -301,16 +269,11 @@ mod tests {
 .........#
 ..........
 .......#..
-#...#.....");
-        println!("{}", universe);
-        println!("expanding...");
+#...#.....",
+        );
         universe.older_expand_universe(100);
         println!("{}", universe);
-        universe.calculate_shortest_paths();
-        let sum: i64 = universe.distances.iter().map(|(_, _, d)| *d as i64).sum();
-        assert_eq!(
-            sum,
-            8410
-        );
+        let sum = universe.calculate_shortest_paths();
+        assert_eq!(sum, 8410);
     }
 }
